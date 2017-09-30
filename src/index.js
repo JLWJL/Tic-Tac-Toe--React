3e14b8f067796448f9d58cf0979a4967e89abc0b
Junlong Wang <wjlinnz@gmail.com>
2017-09-30 21:41:03 +1300

    Add 'calculateWinner' helper function to find winner in each round'

--- src/index.js
+++ src/index.js
@@ -39,7 +39,12 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice(); //'slice' -> copy array, avoid mutability
-    
+
+    //squares[i] avoids changes on existing value of the square
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+
     squares[i] = this.state.xIsNext?"X":"O";
       
     this.setState({
@@ -58,7 +63,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: '+ (this.state.xIsNext?"X":"O");
+
+    const winner = calculateWinner(this.state.squares);
+
+    let status = "";
+    if(winner){
+      status = "Winner is "+winner;
+    }else{
+      
+     status = 'Next player: '+ (this.state.xIsNext?"X":"O");
+    }
 
     return (
       <div>
@@ -98,6 +112,32 @@ class Game extends React.Component {
     );
   }
 }
+
+
+/**
+* Helper functions
+*/
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i = 0; i<lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 // ========================================
 
