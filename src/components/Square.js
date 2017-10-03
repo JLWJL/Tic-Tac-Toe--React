db5a99dbb62754d4baac6ca62ef1b5773e34b873
Junlong Wang <wjlinnz@gmail.com>
2017-10-03 22:25:10 +1300

    Add coloured mark for square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+
+
 export function Square(props){
 	
 	let styles = {backgroundColor:'#00bcd4'}
+	let colour = props.label === 'X'? ' mark-X':' mark-O';
   return(
-    <button className="square" style={props.isWinSquare? styles:null} onClick = {()=> props.onClick()}>
+    <button className={"square"+colour} style={props.isWinSquare? styles:null} onClick = {()=> props.onClick()}>
       {props.label}
     </button>
   );
